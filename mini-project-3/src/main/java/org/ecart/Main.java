9b80ed5293360a8f0f49a6b61846fb50bd3840d3
Cress Franco <crescentf2021@gmail.com>
2024-06-26 13:53:56 +0800

    Added new project
    Added project files for mini-project-3

---
+++ mini-project-3/src/main/java/org/ecart/Main.java
@@ -0,0 +1,9 @@
+package org.ecart;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Test");
+    }
+}
---
+++ mini-project-3/src/main/java/org/ecart/Product.java
@@ -0,0 +1,4 @@
+package org.ecart;
+
+public class Product {
+}
