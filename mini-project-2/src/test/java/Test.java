2da64041f256c3122c33d6618112bc611bf57665
Cress Franco <crescentf2021@gmail.com>
2024-06-26 11:08:00 +0800

    Updated Test.java
    - Added test for user addition
    - Relabeled test names for clarity

--- mini-project-2/src/test/java/Test.java
+++ mini-project-2/src/test/java/Test.java
@@ -1,7 +1,10 @@
+// Written by: Beatriz Franco
+// Test.java
+// Contains test cases
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.example.Book;
-import org.example.BookCollection;
 import org.example.Library;
 
 import org.junit.jupiter.api.Test;
@@ -11,7 +14,7 @@ import java.util.Scanner;
 class MyFirstJUnitJupiterTests {
 
     @Test
-    void addRemove(){
+    void directAddRemove(){
         Library library = new Library();
         Scanner scanner = new Scanner("y\n");
 
@@ -28,23 +31,31 @@ class MyFirstJUnitJupiterTests {
     }
 
     @Test
+    void userAdd(){
+        Library library = new Library();
+        Scanner scanner = new Scanner("book1\nauth1\n1111111111\n");
+
+        library.addBook(scanner);
+
+        // check that a book was added
+        assertEquals(1, library.getBookList().size());
+    }
+
+    @Test
     void search(){
-        // create library and ensure all books are there
+        // create library and ensure all books are added
         Library library = new Library();
         library.addBook(new Book("book1", "auth1", "1-111-11111-1"));
         library.addBook(new Book("book2", "auth2", "2-222-22222-2"));
         library.addBook(new Book("book3", "auth1", "3-333-33333-3"));
         assertEquals(3, library.getBookList().size());
 
-        // create set of books that should come out of search
-        BookCollection testBooks = new BookCollection();
-        testBooks.addBook(library.getBookList().get(0));
-        testBooks.addBook(library.getBookList().get(2));
+        // test for content and number of correct search results
+        assertEquals(library.searchBook("auth1").getBookList().get(0), library.getBookList().get(0));
+        assertEquals(library.searchBook("auth1").getBookList().get(1), library.getBookList().get(2));
+        assertEquals(2, library.searchBook("auth1").getBookList().size());
 
-        // test search function
-        assertEquals(library.searchBook("auth1").getBookList().get(0), testBooks.getBookList().get(0));
-        assertEquals(library.searchBook("auth1").getBookList().get(1), testBooks.getBookList().get(1));
-        assertEquals(library.searchBook("auth1").getBookList().size(), testBooks.getBookList().size());
+        // test for possibility of search with no results
         assertEquals(0, library.searchBook("auth3").getBookList().size());
     }
 
