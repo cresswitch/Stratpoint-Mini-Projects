80e28735154eb93d2c6c275f6a02c3fa9c35c3e4
cresswitch <crescentf2021@gmail.com>
2024-06-27 18:21:28 +0800

    Updated Main.java
    Added user interaction functionality

--- mini-project-3/src/main/java/org/ecart/Main.java
+++ mini-project-3/src/main/java/org/ecart/Main.java
@@ -1,9 +1,30 @@
 package org.ecart;
 
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Cart cart = new Cart();
+        boolean cont = true;
 
-        System.out.println("Test");
+        // user loop
+        while(cont){
+            System.out.println("Enter a to add product to cart, r to remove product, or p to see what's in the cart. Enter anything else to exit.");
+            switch(scanner.nextLine().toLowerCase()){
+                case "a":
+                    cart.addProduct(scanner);
+                    break;
+                case "r":
+                    cart.removeProduct(scanner);
+                    break;
+                case "p":
+                    cart.printTotal();
+                    break;
+                default:
+                    cont = false;
+            }
+        }
     }
 }
