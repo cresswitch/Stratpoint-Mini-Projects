b2267267b1453cc6976f8d49d9427994c32bab53
cresswitch <crescentf2021@gmail.com>
2024-06-27 18:20:07 +0800

    Updated Product.java
    Created Product class and relevant constructors and methods

--- mini-project-3/src/main/java/org/ecart/Product.java
+++ mini-project-3/src/main/java/org/ecart/Product.java
@@ -2,19 +2,22 @@ package org.ecart;
 
 public class Product {
     private int id;
-    private String type;
+    private String name;
     private double cost;
+    private int quantity;
 
     public Product(){
         this.id = 0;
-        this.type = "";
+        this.name = "";
         this.cost = 0;
+        this.quantity = 0;
     }
 
-    public Product(int id, String type, double cost){
+    public Product(int id, String name, double cost){
         this.id = id;
-        this.type = type;
+        this.name = name;
         this.cost = cost;
+        this.quantity = 1;
     }
 
     // setters
@@ -22,24 +25,39 @@ public class Product {
         this.id = id;
     }
 
-    public void setType(String type){
-        this.type = type;
+    public void setName(String name){
+        this.name = name;
     }
 
     public void setCost(double cost){
         this.cost = cost;
     }
 
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
     // getters
     public int getId(){
         return this.id;
     }
 
-    public String getType(){
-        return this.type;
+    public String getName(){
+        return this.name;
     }
 
     public double getCost(){
         return this.cost;
+    }
+
+    public int getQuantity() { return this.quantity; };
+
+    // methods
+    // adds one to quantity
+    public void addQuantity(){
+        quantity++;
+    }
+
+    // removes an indicated number from amount
+    public void removeQuantity(int amount){
+        quantity -= amount;
     }
 }
